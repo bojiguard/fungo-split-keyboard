25bb02c8688d2188bdbc313ba070b2e547d19938
bojiguard <107382014+bojiguard@users.noreply.github.com>
2022-07-01 02:03:23 +0900

    sync files

--- qmk/fungo/rev1/config.h
+++ qmk/fungo/rev1/config.h
@@ -1,20 +1,25 @@
-/* Copyright 2022 bojiguard (@bojiguard)
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+/*
+Copyright 2012 Jun Wako <wakojun@gmail.com>
+Copyright 2015 Jack Humbert
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #pragma once
+
+#ifndef __config_H
+#define __config_H
 
 #include "config_common.h"
 
@@ -218,3 +223,5 @@
 #define MK_3_SPEED
 #define MOUSEKEY_INTERVAL 3
 #define MOUSEKEY_INITIAL_SPEED	20
+
+#endif
--- qmk/fungo/rev1/rev1.h
+++ qmk/fungo/rev1/rev1.h
@@ -1,19 +1,3 @@
-/* Copyright 2022 bojiguard (@bojiguard)
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 #pragma once
 
 #include "fungo.h"
@@ -22,7 +6,7 @@
 
 #ifdef MASTER_RIGHT
 // 우측에  usb 케이블을 꽂아야 정상동작
-#define LAYOUT( \
+#define LAYOUT_fungo_split_num( \
 	L00, L01, L02, L03, L04, L05, L06,                       R06, R05, R04, R03, R02, R01, R00, \
 	L10, L11, L12, L13, L14, L15, L16,                       R16, R15, R14, R13, R12, R11, R10, \
 	L20, L21, L22, L23, L24, L25, L26,                       R26, R25, R24, R23, R22, R21, R20, \
@@ -46,7 +30,7 @@
 #endif
 
 #ifdef MASTER_LEFT
-	#define LAYOUT( \
+	#define LAYOUT_fungo_split_num( \
 	L00, L01, L02, L03, L04, L05, L06,                       R06, R05, R04, R03, R02, R01, R00, \
 	L10, L11, L12, L13, L14, L15, L16,                       R16, R15, R14, R13, R12, R11, R10, \
 	L20, L21, L22, L23, L24, L25, L26,                       R26, R25, R24, R23, R22, R21, R20, \
@@ -71,7 +55,7 @@
 
 /* 왼쪽으로만 연결하면
 #ifdef MASTER_LEFT
-#define LAYOUT( \
+#define LAYOUT_fungo_split_num( \
 	L00, L01, L02, L03, L04, L05, L06,                       R06, R05, R04, R03, R02, R01, R00, \
 	L10, L11, L12, L13, L14, L15, L16,                       R16, R15, R14, R13, R12, R11, R10, \
 	L20, L21, L22, L23, L24, L25, L26,                       R26, R25, R24, R23, R22, R21, R20, \
@@ -99,7 +83,7 @@
 
 #ifdef MASTER_RIGHT
 /*
-#define LAYOUT( \
+#define LAYOUT_fungo_split_num( \
 	L00, L01, L02, L03, L04, L05, L06,                       R06, R05, R04, R03, R02, R01, R00, \
 	L10, L11, L12, L13, L14, L15, L16,                       R16, R15, R14, R13, R12, R11, R10, \
 	L20, L21, L22, L23, L24, L25, L26,                       R26, R25, R24, R23, R22, R21, R20, \
@@ -123,7 +107,7 @@
 */
 
 /*
-#define LAYOUT( \
+#define LAYOUT_fungo_split_num( \
 	L00, L01, L02, L03, L04, L05, L06,                       R06, R05, R04, R03, R02, R01, R00, \
 	L10, L11, L12, L13, L14, L15, L16,                       R16, R15, R14, R13, R12, R11, R10, \
 	L20, L21, L22, L23, L24, L25, L26,                       R26, R25, R24, R23, R22, R21, R20, \
@@ -150,7 +134,7 @@
 
 #ifndef FLIP_HALF
 /*
-#define LAYOUT( \
+#define LAYOUT_fungo_split_num( \
 	L00, L01, L02, L03, L04, L05, L06,                       R06, R05, R04, R03, R02, R01, R00, \
 	L10, L11, L12, L13, L14, L15, L16,                       R16, R15, R14, R13, R12, R11, R10, \
 	L20, L21, L22, L23, L24, L25, L26,                       R26, R25, R24, R23, R22, R21, R20, \
@@ -177,7 +161,7 @@
 // (TRRS jack on both halves are to the right)
 
 /*
-#define LAYOUT( \
+#define LAYOUT_fungo_split_num( \
 	L00, L01, L02, L03, L04, L05, L06,                       R06, R05, R04, R03, R02, R01, R00, \
 	L10, L11, L12, L13, L14, L15, L16,                       R16, R15, R14, R13, R12, R11, R10, \
 	L20, L21, L22, L23, L24, L25, L26,                       R26, R25, R24, R23, R22, R21, R20, \
