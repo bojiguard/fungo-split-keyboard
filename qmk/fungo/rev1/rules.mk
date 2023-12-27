70eb66430b7071d19166221fb12c6c84216017a4
bojiguard <107382014+bojiguard@users.noreply.github.com>
2023-12-27 13:41:55 +0900

    Add files via upload
    change qmk version
    parameter move to json file.
    ref info.json

--- qmk/fungo/rev1/rules.mk
+++ qmk/fungo/rev1/rules.mk
@@ -1,32 +1,17 @@
-# MCU name
-MCU = atmega32u4
-
-# Bootloader selection
-# BOOTLOADER = caterina  # controller pro micro
-BOOTLOADER = atmel-dfu   # controller Elite-C
-
-
 # Build Options
 #   change yes to no to disable
 #
 BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
 MOUSEKEY_ENABLE = yes       # Mouse keys
-EXTRAKEY_ENABLE = yes       # Audio control and System control
+EXTRAKEY_ENABLE = no       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
-COMMAND_ENABLE = no        # Commands for debug and configuration
+COMMAND_ENABLE = no       # Commands for debug and configuration
 NKRO_ENABLE = no            # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
 KEY_LOCK_ENABLE = yes       # kc_lock use
-ENCODER_ENABLE = yes        # encoder
-# ENCODER_MAP_ENABLE = yes    # ENCODER MAP
-
-# caps word, auto return prev capslock,  
-# check option {COMMAND_ENABLE = no}
-CAPS_WORD_ENABLE = yes
 
 
 OLED_ENABLE = no
-OLED_DRIVER = SSD1306      # OLED display
 SPLIT_KEYBOARD = yes       # split type
