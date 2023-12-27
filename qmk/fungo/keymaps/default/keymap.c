6f26c56d7db04937e21d7cdb91a7627233af8e72
bojiguard <107382014+bojiguard@users.noreply.github.com>
2023-12-27 13:52:37 +0900

    change layer const
    for qmk new version

--- qmk/fungo/keymaps/default/keymap.c
+++ qmk/fungo/keymaps/default/keymap.c
@@ -21,7 +21,7 @@ enum layer_names {
 		SAM1,
 		SAM2,
 		SAM3,
-		SAM4
+		SAM4,
 };
 
 
@@ -50,6 +50,13 @@ enum layer_names {
 //#define MTLC_8   MT(MOD_LCTL, KC_8)      
 #define MTLC_9   MT(MOD_LSFT, KC_9)
 
+#define LSFT_CAPS    MT(MOD_LSFT,KC_CAPS)
+#define RSFT_CAPS    MT(MOD_RSFT,KC_CAPS)
+#define LT2SPC    LT(SAM2, KC_SPC)
+#define LT3SPC    LT(SAM3, KC_SPC)
+#define LGUSPC    MT(MOD_LGUI, KC_SPC)
+#define RGUSPC    MT(MOD_RGUI, KC_SPC)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* BASE
@@ -66,7 +73,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `------------------------------------------------'                                ------------------------------------------------'
    */
 /*
-  [BASE] = LAYOUT_fungo_split_num(
+  [BASE] = LAYOUT(
     KC_GRV,     KC_1,     KC_2,    KC_3,    KC_4,     KC_5,    KC_ESC,                                                              KC_BSPC,  KC_6,  KC_7,   KC_8,    KC_9,    KC_0,    KC_NO,
     KC_TAB,     KC_Q,     KC_W,    KC_E,    KC_R,     KC_T,    KC_MINS,                                                             KC_EQL,   KC_Y,  KC_U,   KC_I,    KC_O,    KC_P,    KC_TAB,
     KC_NO,      KC_A,     SFCT_S,  CT_D,    SF_F,     KC_G,    KC_LBRC,                                                             KC_RBRC,  KC_H,  MTC_J,  MTC_K,   KC_L,    KC_SCLN, KC_NO,
@@ -88,31 +95,34 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `------------------------------------------------'                                ------------------------------------------------'
    */
 /*
-  [F3LAY] = LAYOUT_fungo_split_num(
+  [F3LAY] = LAYOUT(
     KC_F11,     KC_F1,      KC_F2,       KC_F3,        KC_F4,        KC_F5,   KC_ESC,                                                              KC_BSPC, KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_F12,
     KC_TAB,     KC_1,       KC_2,        KC_3,         KC_4,         KC_5,    KC_CAPS,                                                             KC_CAPS, KC_6,    KC_7,    KC_8,    KC_9,     KC_0,    KC_TAB,
     KC_NO,      KC_NO,      SFCTLNO,     KC_LCTL,      KC_LSFT,      KC_PAUS, KC_PSCR,                                                             KC_BSPC, KC_DEL,  KC_HOME, KC_UP,   KC_END,   KC_PGUP, KC_NO,
     KC_LCTL,    LCTL(KC_Z), LCTL(KC_X),  LCTL(KC_C),   LCTL(KC_V),   KC_NO,   KC_NO,    LM(F3LAY, MOD_LSFT), KC_NO,   KC_NO,  LM(F3LAY, MOD_RSFT), KC_NO,   KC_INS,  KC_LEFT, KC_DOWN, KC_RIGHT, KC_PGDN, KC_RCTL,
-    KC_NO,      KC_NO,      KC_NO,       KC_LALT,           KC_LSHIFT,        KC_SPC,   MO(F3LAY),           KC_ENT,  KC_ENT, TT(F3LAY),           KC_SPC,     KC_RSHIFT,     KC_RALT, KC_NO,    KC_NO,   TG(F4LAY)
+    KC_NO,      KC_NO,      KC_NO,       KC_LALT,           KC_LSHIFT,        KC_SPC,   MO(F3LAY),           KC_ENT,  KC_ENT, TT(F3LAY),           KC_SPC,     KC_RSHIFT,     KC_RALT, KC_NO,    KC_NO,   TG(F4LAY),
+                                                                                                             KC_BTN2, KC_BTN1
   ),
 
 
-  [F4LAY] = LAYOUT_fungo_split_num(
+  [F4LAY] = LAYOUT(
     KC_F11,     KC_F1,      KC_F2,       KC_F3,        KC_F4,        KC_F5,   KC_ESC,                                                              KC_BSPC, KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_F12,
     KC_TAB,     KC_NO,      KC_NO,       KC_NO,        KC_NO,        KC_NO,   KC_CAPS,                                                             KC_CAPS, KC_DEL,  KC_HOME, KC_UP,   KC_END,   KC_NO,   KC_TAB,
     KC_NO,      KC_NO,      SFCTLNO,     KC_LCTL,      KC_LSFT,      KC_PAUS, KC_PSCR,                                                             KC_BSPC, KC_INS,  KC_LEFT, KC_DOWN, KC_RIGHT, KC_NO,   KC_NO,
     KC_LCTL,    LCTL(KC_Z), LCTL(KC_X),  LCTL(KC_C),   LCTL(KC_V),   KC_NO,   KC_NO,    LM(F3LAY, MOD_LSFT), KC_NO,   KC_NO,  LM(F3LAY, MOD_RSFT), KC_NO,   KC_INS,  KC_LEFT, KC_DOWN, KC_RIGHT, KC_PGDN, KC_RCTL,
-    KC_NO,      KC_NO,      KC_NO,       KC_LALT,           KC_LSHIFT,        KC_SPC,   MO(F3LAY),           KC_ENT,  KC_ENT, TT(F3LAY),           KC_SPC,     KC_RSHIFT,     KC_RALT, KC_NO,    KC_NO,   TG(F4LAY)
+    KC_NO,      KC_NO,      KC_NO,       KC_LALT,           KC_LSHIFT,        KC_SPC,   MO(F3LAY),           KC_ENT,  KC_ENT, TT(F3LAY),           KC_SPC,     KC_RSHIFT,     KC_RALT, KC_NO,    KC_NO,   TG(F4LAY),
+                                                                                                             KC_BTN2, KC_BTN1
   ),
 */
   
   /*
-    [TEST] = LAYOUT_fungo_split_num(
+    [TEST] = LAYOUT(
     KC_GRV,     KC_1,   KC_2,    KC_3,    KC_4,     KC_5,    KC_ESC,                                                KC_BSPC,  KC_6,    KC_7,    KC_8,      KC_9,   KC_0,    KC_NO,
     KC_TAB,     KC_Q,   KC_W,    KC_E,    KC_R,     KC_T,    KC_MINS,                                               KC_EQL,   KC_Y,    KC_U,    KC_I,      KC_O,   KC_P,    KC_TAB,
     KC_LSFT,    KC_A,   KC_S,    KC_D,    KC_F,     KC_G,    KC_LBRC,                                               KC_RBRC,  KC_H,    KC_J,    KC_K,      KC_L,   KC_SCLN, KC_RSFT,
     KC_LCTL,    KC_Z,   KC_X,    KC_C,    KC_V,     KC_B,    KC_BSLS,  KC_LCTL,        KC_NO,   KC_NO,  TG(F4LAY),  KC_QUOT,  KC_N,    KC_M,    KC_COMM,   KC_DOT, KC_SLSH, KC_RCTL,
-    KC_NO,      KC_NO,  KC_NO,   KC_LALT,    KC_LSHIFT,      KC_SPC,   MO(F3LAY),      KC_ENT,  KC_ENT, TT(F3LAY),  KC_SPC,      KC_RSHIFT,     KC_RALT,   KC_NO,  KC_NO,   KC_NO
+    KC_NO,      KC_NO,  KC_NO,   KC_LALT,    KC_LSHIFT,      KC_SPC,   MO(F3LAY),      KC_ENT,  KC_ENT, TT(F3LAY),  KC_SPC,      KC_RSHIFT,     KC_RALT,   KC_NO,  KC_NO,   KC_NO,
+                                                                                                             KC_BTN2, KC_BTN1
   ),
   */
   
@@ -121,38 +131,46 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // A(KC_LEFT) : BROWER BACKWARD, A(KC_RIGHT): BROWER FORWARD
   // KC_LANGUAGE_1 	KC_LNG1 	Hangul/English
   // KC_LANGUAGE_2 	KC_LNG2 	Hanja
+  // 0번 레이어의 캡스락을 시프트키로 변경, 캡스락은 1번 오른쪽 레어어만 사용.  22.10.24
+  // 왼쪽 컨트롤,시프트키 조합(에스 키)는 삭제함, 오타발생이 많아서 기능키가 작동됨. 22.10.27
+  // RSFT_CAPS 추가
+  // 언어변경후 클리어 콤보기능 추가 -> 왼쪽 컨트롤키 + 오른쪽 랭기지
 
-    [SAM1] = LAYOUT_fungo_split_num(
-    KC_NO,      KC_1,        KC_2,       KC_3,         KC_4,         KC_5,    KC_ESC,                                               KC_ESC,   KC_6,  KC_7,   KC_8,    KC_9,    KC_0,     KC_NO,
-    KC_NO,      KC_Q,        KC_W,       KC_E,         KC_R,         KC_T,    KC_BSPC,                                              KC_BSPC,  KC_Y,  KC_U,   KC_I,    KC_O,    KC_P,     KC_NO,
-    KC_TAB,     KC_A,        SFCT_S,     CT_D,         SF_F,         KC_G,    KC_CAPS,                                              KC_CAPS,  KC_H,  MTC_J,  MTC_K,   KC_L,    KC_SCLN,  KC_TAB,
-    KC_LCTL,    KC_Z,        KC_X,       KC_C,         KC_V,         KC_B,    A(KC_LEFT),   TT(SAM3), MO(SAM4), MO(SAM4),TT(SAM2),  KC_LNG1,  KC_N,  KC_M,   KC_COMM, KC_DOT,  KC_SLSH,  KC_RCTL,
-    KC_NO,      TT(SAM1),    KC_NO,      KC_LALT,             KC_SPC,         TT(SAM2),     KC_LCTL,  KC_ENT,   KC_ENT,  KC_RCTL,   TT(SAM3),     KC_SPC,    KC_RALT, KC_NO,   TT(SAM1), KC_NO
+    [SAM1] = LAYOUT(
+    KC_NO,      KC_1,        A(KC_LEFT), KC_ENT,       LSFT_CAPS,    KC_BSPC, KC_ESC,                                                 KC_ESC,   KC_BSPC, RSFT_CAPS,   KC_ENT,    KC_LNG1,    KC_0,     KC_NO,
+    KC_NO,      KC_Q,        KC_W,       KC_E,         KC_R,         KC_T,    KC_BSPC,                                                KC_BSPC,    KC_Y,  KC_U,   KC_I,    KC_O,    KC_P,     KC_NO,
+    KC_TAB,     KC_A,        KC_S,       CT_D,         SF_F,         KC_G,    LSFT_CAPS,                                              RSFT_CAPS,  KC_H,  MTC_J,  MTC_K,   KC_L,    KC_SCLN,  KC_TAB,
+    KC_LCTL,    KC_Z,        KC_X,       KC_C,         KC_V,         KC_B,    A(KC_LEFT),   TT(SAM3), TT(SAM4),  TT(SAM4), TT(SAM2),  KC_LNG1,    KC_N,  KC_M,   KC_COMM, KC_DOT,  KC_SLSH,  KC_RCTL,
+    KC_NO,      TT(SAM1),    KC_NO,      KC_LALT,             KC_SPC,         TT(SAM2),     KC_LCTL,  KC_LGUI,     KC_PSCR,    KC_RCTL,   TT(SAM3),     KC_SPC,      KC_RALT, KC_NO,   KC_8,     KC_9,
+                                                                                                             KC_BTN1, KC_BTN2
   ),
   
-    [SAM2] = LAYOUT_fungo_split_num(
-    KC_NO,      KC_NO,      KC_NO,       KC_NO,        KC_NO,        KC_NO,   KC_ESC,                                                KC_ESC,  KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,
-    KC_NO,      KC_F1,      KC_F2,       KC_F3,        KC_F4,        KC_F5,   KC_F11,                                                KC_BSPC, KC_DEL,  KC_HOME, KC_UP,   KC_END,   KC_PGUP, KC_NO,
-    KC_TAB,     KC_F6,      MTC_F7,      MTC_F8,       MTC_F9,       KC_F10,  KC_F12,                                                KC_CAPS, KC_INS,  KC_LEFT, KC_DOWN, KC_RIGHT, KC_PGDN, KC_TAB,
-    KC_LCTL,    LCTL(KC_Z), LCTL(KC_X),  LCTL(KC_C),   LCTL(KC_V),   KC_NO,   A(KC_RIGHT),  TT(SAM3), MO(SAM4), MO(SAM4), TT(SAM2),  KC_LNG2, KC_BSLS, KC_QUOT, KC_LBRC, KC_RBRC,  KC_GRV,  KC_RCTL,
-    KC_NO,      TT(SAM1),   KC_NO,       KC_LALT,             KC_SPC,         TT(SAM2),     KC_LCTL,  KC_ENT,    KC_ENT,  KC_RCTL,   TT(SAM3),    KC_SPC,       KC_RALT, KC_NO,   TT(SAM1), KC_NO
+    [SAM2] = LAYOUT(
+    DF(SAM1),   KC_NO,      KC_NO,       KC_NO,        KC_NO,        KC_NO,   LALT(LCTL(KC_DEL)),                                     KC_ESC,  LCTL(KC_X), LCTL(KC_C), LCTL(KC_V),  LCTL(KC_Z), KC_NO,    DF(SAM1),
+    KC_NO,      KC_F1,      KC_F2,       KC_F3,        KC_F4,        KC_F5,   KC_F11,                                                 KC_BSPC, KC_DEL,     KC_HOME,    KC_UP,       KC_END,     KC_PGUP,  KC_NO,
+    KC_TAB,     KC_F6,      KC_F7,       MTC_F8,       MTC_F9,       KC_F10,  KC_F12,                                                 KC_CAPS, KC_INS,     KC_LEFT,    KC_DOWN,     KC_RIGHT,   KC_PGDN,  KC_TAB,
+    KC_LCTL,    LCTL(KC_Z), LCTL(KC_X),  LCTL(KC_C),   LCTL(KC_V),   KC_NO,   A(KC_RIGHT),  TT(SAM3),  TT(SAM4),  TT(SAM4), TT(SAM2), KC_LNG2, KC_BSLS,    KC_QUOT,    KC_LBRC,     KC_RBRC,    KC_GRV,   KC_RCTL,
+    KC_NO,      TT(SAM1),   KC_NO,       KC_LALT,             KC_SPC,         TT(SAM2),     KC_LCTL,   KC_LGUI,     KC_PSCR,    KC_RCTL,  TT(SAM3),       KC_SPC,          KC_RALT,     KC_NO,      TT(SAM1), KC_NO,
+                                                                                                             KC_BTN1, KC_BTN2
   ),  
 
-    [SAM3] = LAYOUT_fungo_split_num(
-    KC_NO,      KC_NO,      KC_NO,       KC_NO,        KC_NO,        KC_NO,   KC_ESC,                                             KC_ESC,  KC_NO,   KC_NO,     KC_NO,   KC_NO,    KC_NO,    KC_NO,
-    KC_NO,      KC_1,       KC_2,        KC_3,         KC_4,         KC_5,    KC_BSPC,                                            KC_BSPC, KC_1,    KC_2,      KC_3,    KC_4,     KC_5,     KC_NO,
-    KC_TAB,     KC_6,       KC_7,        KC_8,         MTLC_9,       KC_0,    CAPSWRD,                                            CAPSWRD, KC_6,    MTC_7,     MTC_8,   KC_9,     KC_0,     KC_TAB,
-    KC_LCTL,    KC_PSLS,    KC_PAST,     KC_MINS,      KC_PPLS,      KC_EQL,  KC_DOT,   TT(SAM3), MO(SAM4), MO(SAM4),  TT(SAM2),  KC_DOT,  KC_EQL,  KC_PPLS,   KC_PMNS, KC_PAST,  KC_PSLS,  KC_RCTL,
-    KC_NO,      TT(SAM1),   KC_NO,       KC_LALT,             KC_SPC,         TT(SAM2), KC_LCTL,  KC_ENT,   KC_ENT,    KC_RCTL,   TT(SAM3),    KC_SPC,         KC_RALT, KC_NO,    TT(SAM1), KC_NO
+    [SAM3] = LAYOUT(
+    DF(SAM1),   KC_NO,      KC_NO,       KC_NO,        KC_NO,        KC_NO,   LALT(LCTL(KC_DEL)),                                  KC_ESC,  KC_NO,   KC_NO,     KC_NO,   KC_NO,    KC_NO,    DF(SAM1),
+    KC_NO,      KC_1,       KC_2,        KC_3,         KC_4,         KC_5,    KC_BSPC,                                             KC_BSPC, KC_1,    KC_2,      KC_3,    KC_4,     KC_5,     KC_NO,
+    KC_TAB,     KC_6,       KC_7,        KC_8,         MTLC_9,       KC_0,    KC_DOT,                                              KC_DOT,  KC_6,    MTC_7,     MTC_8,   KC_9,     KC_0,     KC_TAB,
+    KC_LCTL,    KC_PSLS,    KC_PAST,     KC_MINS,      KC_PPLS,      KC_EQL,  KC_COMM,  TT(SAM3),  TT(SAM4),  TT(SAM4), TT(SAM2),  KC_COMM, KC_EQL,  KC_PPLS,   KC_PMNS, KC_PAST,  KC_PSLS,  KC_RCTL,
+    KC_NO,      TT(SAM1),   KC_NO,       KC_LALT,             KC_SPC,         TT(SAM2), KC_LCTL,   KC_LGUI,     MOD_RGUI,    KC_PSCR,   TT(SAM3),    KC_SPC,         KC_RALT, KC_NO,    TT(SAM1), KC_NO,
+                                                                                                             KC_BTN1, KC_BTN2
   ),
 
 
-    [SAM4] = LAYOUT_fungo_split_num(
-    KC_NO,      KC_NO,      KC_NO,       KC_NO,        KC_NO,        KC_NO,   KC_ESC,                                            KC_ESC,   KC_NO,     KC_NO,   KC_NO,   KC_NO,    KC_NO,    KC_NO,
-    KC_NO,      KC_F1,      KC_F2,       KC_F3,        KC_F4,        KC_F5,   KC_F11,                                            KC_BSPC,  KC_ACL0,   KC_BTN1, KC_MS_U, KC_BTN2,  KC_NO,    KC_NO,
-    KC_TAB,     KC_F6,      MTC_F7,      MTC_F8,       MTC_F9,       KC_F10,  KC_F12,                                            KC_CAPS,  KC_ACL1,   KC_MS_L, KC_MS_D, KC_MS_R,  KC_NO,    KC_TAB,
-    KC_NO,      KC_NO,      KC_ACL0,     KC_ACL1,      KC_ACL2,      KC_NO,   KC_NO,    TT(SAM3), MO(SAM4), MO(SAM4), TT(SAM2),  KC_NO,    KC_ACL2,   KC_WH_L, KC_WH_U, KC_WH_D,  KC_WH_R,  KC_RCTL,
-    KC_NO,      TT(SAM1),   KC_NO,       KC_LALT,             KC_SPC,         TT(SAM2), KC_LCTL,  KC_ENT,   KC_ENT,   KC_RCTL,   TT(SAM3),       KC_SPC,       KC_RALT, KC_NO,    TT(SAM1), KC_NO
+    [SAM4] = LAYOUT(
+    DF(SAM1),   KC_NO,      KC_NO,       KC_NO,        KC_NO,        KC_NO,   KC_ESC,                                             KC_ESC,   KC_NO,     KC_NO,   KC_NO,   KC_NO,    KC_NO,    DF(SAM1),
+    KC_NO,      KC_NO,      KC_NO,       KC_WH_U,      KC_BTN2,      KC_BTN1, KC_NO,                                              KC_NO,    KC_ACL0,   KC_BTN1, KC_BTN2, KC_BTN3,  KC_BTN4,  KC_NO,
+    KC_NO,      KC_NO,      KC_WH_L,     KC_WH_D,      KC_WH_R,      KC_NO,   KC_NO,                                              KC_NO,    KC_ACL1,   KC_MS_L, KC_MS_D, KC_MS_U,  KC_MS_R,  KC_TAB,
+    KC_NO,      KC_NO,      KC_ACL0,     KC_ACL1,      KC_ACL2,      KC_NO,   KC_NO,    TT(SAM3),  TT(SAM4), TT(SAM4), TT(SAM2),  KC_NO,    KC_ACL2,   KC_WH_L, KC_WH_D, KC_WH_U,  KC_WH_R,  KC_RCTL,
+    KC_NO,      TT(SAM1),   KC_NO,       KC_LALT,             KC_SPC,         TT(SAM2), KC_LCTL,   KC_LGUI,    MOD_RGUI,    KC_PSCR,   TT(SAM3),       KC_SPC,       KC_RALT, KC_NO,    TT(SAM1), KC_NO,
+                                                                                                             KC_BTN1, KC_BTN2
   )
 };
 
@@ -189,11 +207,41 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 #ifdef CONSOLE_ENABLE
 //    uprintf("KL: kc: 0x%04X, col: %u, row: %u, pressed: %b, time: %u, interrupt: %b, count: %u\n", keycode, record->event.key.col, record->event.key.row, record->event.pressed, record->event.time, record->tap.interrupted, record->tap.count);
 #endif 
+
+    switch (keycode) {
+		case KC_LNG1:
+			// Detect the activation of only Left shift
+			if ((get_mods() & MOD_BIT(KC_LCTL)) == MOD_BIT(KC_LCTL)) {
+
+				unregister_code(KC_LCTL);
+				register_code(KC_LCTL);
+				register_code(KC_LSFT);
+				tap_code16(KC_LEFT);
+				unregister_code(KC_LCTL);
+				unregister_code(KC_LSFT);
+				tap_code16(KC_BSPC);
+				//tap_code16(KC_LNG1);
+			}
+			// Else, let QMK process the KC_ESC keycode as usual
+			return true;
+    }
   return true;
 }
 
 
 // encoder
+
+#if defined(ENCODER_MAP_ENABLE)
+
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
+    [0] = { ENCODER_CCW_CW(KC_MS_WH_UP, KC_MS_WH_DOWN), },
+    [1] = { ENCODER_CCW_CW(KC_UP, KC_DOWN), },
+    [2] = { ENCODER_CCW_CW(KC_PGUP, KC_PGDN), },
+    [3] = { ENCODER_CCW_CW(KC_LEFT, KC_RIGHT), },
+};
+#endif
+
+/*
 bool encoder_update_user(uint8_t index, bool clockwise) {
     switch(get_highest_layer(layer_state|default_layer_state)) {
         case 0:
@@ -231,3 +279,49 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
     }
     return false;
 }
+*/
+
+/*
+enum combo_events {
+  LANG_CLEAR,
+  COMBO_LENGTH
+};
+uint16_t COMBO_LEN = COMBO_LENGTH; // remove the COMBO_COUNT define and use this instead!
+const uint16_t PROGMEM lang_clear_combo[] = {KC_RCTL, KC_LNG1, COMBO_END};
+
+
+combo_t key_combos[] = {
+  [LANG_CLEAR] = COMBO_ACTION(lang_clear_combo),
+  //[BSPC_LSFT_CLEAR] = COMBO_ACTION(clear_line_combo),
+};
+*/
+
+/* COMBO_ACTION(x) is same as COMBO(x, KC_NO) */
+
+/*
+void process_combo_event(uint16_t combo_index, bool pressed) {
+  switch(combo_index) {
+	  
+    //case LANG_CLEAR:
+    //  if (pressed) {
+    //    SEND_STRING("john.doe@example.com");
+    //  }
+    //  break;
+	  
+    case LANG_CLEAR:
+      if (pressed) {
+        //tap_code16(KC_END);
+		//tap_code16(SC(KC_HOME));
+		
+		register_code(KC_RCTL);
+		register_code(KC_RSHIFT);
+        tap_code16(KC_LEFT);
+		unregister_code(KC_RCTL);
+		unregister_code(KC_RSHIFT);
+		tap_code16(KC_BSPC);
+		tap_code16(KC_LNG1);
+      }
+      break;
+  }
+}
+*/
